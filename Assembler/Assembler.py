02a46384c824caee574c2d92dbebb4dec4e56e9c
max-wickham <mw1919@ic.ac.uk>
2020-12-07 17:53:36 +0000

    Assembler

--- Assembler/Assembler.py
+++ Assembler/Assembler.py
@@ -11,8 +11,42 @@ def bitString(x,bits):
     return format(x, '0' + str(bits) + 'b')
 
 def reg(reg_code):
-    reg_code = reg_code[1:]
-    output = format(int(reg_code),'05b')
+    registers = {"$zero":0,
+        "$at":1,
+        "$v0":2,
+        "$v1":3,
+        "$a0":4,
+        "$a1":5,
+        "$a2":6,
+        "$a3":7,
+        "$t0":8,
+        "$t1":9,
+        "$t2":10,
+        "$t3":11,
+        "$t4":12,
+        "$t5":13,
+        "$t6":14,
+        "$t7":15,
+        "$s0":16,
+        "$s1":17,
+        "$s2":18,
+        "$s3":19,
+        "$s4":20,
+        "$s5":21,
+        "$s6":22,
+        "$s7":23,
+        "$t8":24,
+        "$t9":25,
+        "$k0":26,
+        "$k1":27,
+        "$gp":28,
+        "$sp":29,
+        "$fp":30,
+        "$ra":31
+    }
+    
+    reg_code = registers[reg_code.lower()]
+    output = format(reg_code,'05b')
     return output
 
 def decode(line,parameters,line_names):
@@ -102,11 +136,10 @@ def decode(line,parameters,line_names):
         #elif opcode in jmp_type:
         elif opcodes[opcode][0] == "j":
             reg1 = line[1]
-            reg2 = line[2]
-            const = line[3]
+            const = line[2]
             output += opcodes[opcode][1]
             output += reg(reg1)
-            output += reg(reg2)
+            output += branch_codes[opcode]
             if isDigit(const):
                 output += bitString(int(const),16)
             elif output in parameters:
@@ -201,7 +234,8 @@ def writeOutput(output,output_file):
     print(output_string)
 
 
-f = open(input(), 'r')
+#f = open(input(), 'r')
+f = open("test.txt", 'r')
 lines = f.readlines()
 
 parameters = {}
