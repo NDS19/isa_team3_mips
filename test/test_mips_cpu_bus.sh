f96dc2d72709ffa3887402f5d969335690966c7e
NDS19 <ndasilva115@gmail.com>
2020-12-20 18:38:08 +0000

    Script update; fixed the repeat instruction names, cleaned up the comments

--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -27,9 +27,12 @@ set -eou pipefail
 # 7. Adjust directory and script to compile every verilog file in the mips_cpu
 #     folder [tick]
 # 8. Send relative outputs from running the script to stderr rather than stdout
-#     This means things like the output of diff [possible tick, check sanity check]
+#     This means things like the output of diff [tick]
 # 9. Might need a stderr output on the compile line (update based on the Friday sanity check) [not needed]
 # 10. Ensure all instructions have a test case with the _0 suffix
+# 11. Fix unsigned output for the link instructions
+# 12. Fix test-case output for when the instruction == and, repeatedely outputting
+#   andi_0 and Pass etc.
 
 # Possible future improvements
 # 1. The for loop going through each instruction test case could be summarised
@@ -62,29 +65,16 @@ if [ $# -eq 2 ] ; then # if there are two input arguments
     # with the suffix .asm.txt e.g. addiu_1.asm.txt
     # The * selects all different test-cases of the instruction
 
-    # Extracting the test-names from the testcases
-
-    #for i in ${TESTCASES} ; do
-        # Extract just the testcase name from the filename. See `man basename` for what this command does.
-        # Removing the .asm.txt suffix
-    #    TESTNAME=$(basename ${i} .asm.txt)
-        #TESTNAME now just contains the test-case name e.g. addiu_1
-    #done
-
     >&2 echo "Testing CPU from : ${source_directory} with instruction : ${instruction}"
 
     for i in ${TESTCASES} ; do
-        # echo ${i}
     # iterating through the test cases, i represents the file name e.g. addiu_1
-    #for i in ${TESTCASES} ; do
         TESTNAME=$(basename ${i} .asm.txt)
         # removing the .asm.txt suffix and directory prefix
-        #echo ${TESTNAME}
-      # start assembly for testing a single instruction
+        # start assembly for testing a single instruction
         >&2 echo " 1 - Assembling test files for ${TESTNAME}"
         echo test/0-instructions_assembly/${TESTNAME}.asm.txt | python3 Assembler/Assembler.py \
         > test/1-binary/${TESTNAME}.hex.txt
-        #done
         #>&2 echo " Successfully assembled test files for ${instruction}"
         # instruction assembly files have now been compiled into their hex binary files
 
@@ -92,8 +82,7 @@ if [ $# -eq 2 ] ; then # if there are two input arguments
         # compile all components needed for the test-bench and forming an
         # executable file which runs the CPU with the instructions input into
         # RAM;
-    #for i in ${TESTCASES} ; do
-        #TESTNAME=$(basename ${i} .asm.txt)
+
         # -P is used to adjust the parameter in the testbench verilog so we can
         # input a file that is read in
         iverilog -g 2012 \
@@ -104,18 +93,11 @@ if [ $# -eq 2 ] ; then # if there are two input arguments
         -P mips_cpu_bus_tb.INSTRUCTION=\"${instruction}\" \
         -o test/2-simulator/CPU_MU0_bus_tb_${TESTNAME} # set the test-bench as top level since this instantiates everything # having the test case file input into the RAM
         # output executable file for this instruction testcase
-        # done
-    # MAKE SURE TO ADJUST THIS BLOCK OF CODE FOR POSSIBLE CHANGES IN DIRECTORY
-    # >&2 echo " Successfully compiled test-bench"
+        # MAKE SURE TO ADJUST THIS BLOCK OF CODE FOR POSSIBLE CHANGES IN DIRECTORY
 
-    >&2 echo " 3 - Running the test-bench for ${TESTNAME}"
-    # Running the executable cpu file we just made and capturing the output
-    # display instructions to an output file
-    # If we just use $display for the OUT instruction, we can output the value
-    # at the destination register
-
-    #for i in ${TESTCASES} ; do
-    #  TESTNAME=$(basename ${i} .asm.txt)
+        >&2 echo " 3 - Running the test-bench for ${TESTNAME}"
+        # Running the executable cpu file we just made and capturing the output
+        # display instructions to an output file
         set +e
 
         test/2-simulator/CPU_MU0_bus_tb_${TESTNAME} > test/3-output/CPU_MU0_bus_${TESTNAME}.stdout
@@ -126,22 +108,19 @@ if [ $# -eq 2 ] ; then # if there are two input arguments
 
         if [[ "${RESULT}" -ne 0 ]] ; then
           # fail condition
-          # need to find a way to obtain the word 'instruction'
            echo "${TESTNAME} ${instruction} Fail"
            continue
-           #exit TODO recover this exit code when the CPU is functioning
+           #exit TODO, might not need to recover this exit code
         fi
 
         # we now need to extract the necessary lines with the prefix "RESULT : "
         # The associated value is then output and written into a .out file
         # so that we can compare it to the reference output result files
-        # >&2 echo " Extracting final results"
         PATTERN="RESULT : "
         NOTHING=""
 
         set +e
-        # grep grabs the lines with the prefix described by PATTERN and outputs them to
-        # a new file
+        # grep grabs the lines with the prefix described by PATTERN and outputs them to a new file
         grep "${PATTERN}" test/3-output/CPU_MU0_bus_${TESTNAME}.stdout > \
         test/3-output/CPU_MU0_bus_${TESTNAME}.result-lines
 
@@ -152,16 +131,11 @@ if [ $# -eq 2 ] ; then # if there are two input arguments
         # Actual final result of the test case is stored in a .out file
         # Note that the output of this will have spaces before the actual value but
         # this shouldn't have an effect when comparing to the reference files
-    #done
-
-    #>&2 echo " Successfully ran the test-bench"
 
         >&2 echo " 4 - Comparing to the reference output files for ${TESTNAME}"
-    # Here we compare the generated output files from part 3 with the pre-generated
-    # output files in 4-reference
+        # Here we compare the generated output files from part 3 with the pre-generated
+        # output files in 4-reference
 
-    #for i in ${TESTCASES} ; do
-        #TESTNAME=$(basename ${i} .asm.txt)
         set +e # +e used to stop the script failing if an error occurs
         >&2 diff -w test/4-reference/${TESTNAME}.out test/3-output/CPU_MU0_bus_${TESTNAME}.out
         RESULT=$? # output of this diff line stored in RESULT
@@ -175,21 +149,20 @@ if [ $# -eq 2 ] ; then # if there are two input arguments
           # pass condition
            echo "${TESTNAME} ${instruction} Pass"
         fi
-    #done
     done
 elif [ $# -eq 1 ] ; then  # if nothing is specified for $2, all test-cases should be run
-    >&2 echo "Testing CPU : ${source_directory} for all instructions"
+    >&2 echo "Testing CPU in ${source_directory} for all instructions"
 
     INSTRUCTIONS="test/0-instructions_assembly/*_0.asm.txt"
-    # Holds every instruction with the prefix _1/asm.txt therefore holding every
+    # Holds EVERY INSTRUCTION with the prefix _1/asm.txt therefore holding every
     # instruction assuming every instruction has the _0 suffix
     for i in ${INSTRUCTIONS} ; do
         instruction=$(basename ${i} _0.asm.txt)
-        # Removes the _0.asm.txt to just get the instruction name
-        TESTCASES="test/0-instructions_assembly/${instruction}*.asm.txt"
-        # TESTCASES holds all the test files for the current instruction
-        # with the suffix .asm.txt; for the addiu instruction, it holds addiu_1,
-        # addiu_2 and addiu_3
+        # Removes the _0.asm.txt and the directory path to just get the instruction name
+        TESTCASES="test/0-instructions_assembly/${instruction}_*.asm.txt"
+        # TESTCASES holds all the test files for the CURRENT instruction
+        # with the suffix .asm.txt; for the addiu instruction, it holds addiu_0,
+        # addiu_1, addiu_2 etc.
         # We need to remove the suffix at every loop where we access the TESTCASES
 
         # start assembly for testing for all instructions
@@ -200,16 +173,12 @@ elif [ $# -eq 1 ] ; then  # if nothing is specified for $2, all test-cases shoul
             TESTNAME=$(basename ${i} .asm.txt)
             echo test/0-instructions_assembly/${TESTNAME}.asm.txt | python3 Assembler/Assembler.py \
             >test/1-binary/${TESTNAME}.hex.txt
-        # done
-            # >&2 echo " Successfully assembled test files"
-        # instruction assembly files have now been compiled into their hex binary files
+            # instruction assembly files have now been compiled into their hex binary files
 
             >&2 echo " 2 - Compiling test-bench for ${TESTNAME}"
-        # compile all components needed for the test-bench and forming an
-        # executable file which runs the CPU with the instructions input into
-        # RAM;
-        #for i in ${TESTCASES} ; do
-        #    TESTNAME=$(basename ${i} .asm.txt)
+            # compile all components needed for the test-bench and forming an
+            # executable file which runs the CPU with the instructions input into
+            # RAM;
             iverilog -g 2012 \
             ${source_directory}/mips_cpu_*.v test/mips_cpu_bus_tb.v test/RAM_8x4096.v \
             ${source_directory}/mips_cpu/*.v \
@@ -217,17 +186,11 @@ elif [ $# -eq 1 ] ; then  # if nothing is specified for $2, all test-cases shoul
             -P mips_cpu_bus_tb.RAM_INIT_FILE=\"test/1-binary/${TESTNAME}.hex.txt\" \
             -P mips_cpu_bus_tb.INSTRUCTION=\"${instruction}\" \
             -o test/2-simulator/CPU_MU0_bus_tb_${TESTNAME} # output executable file for this instruction testcase
-        #done
-        #>&2 echo " Successfully compiled test-bench"
 
             >&2 echo " 3 - Running the test-bench for ${TESTNAME}"
-        # Running the executable cpu file we just made and capturing the output
-        # display instructions to an output file
-        # If we just use $display for the OUT instruction, we can output the value
-        # at the destination register
+            # Running the executable cpu file we just made and capturing the output
+            # display instructions to an output file
 
-        #for i in ${TESTCASES} ; do
-        #    TESTNAME=$(basename ${i} .asm.txt)
             set +e
 
             test/2-simulator/CPU_MU0_bus_tb_${TESTNAME} > test/3-output/CPU_MU0_bus_${TESTNAME}.stdout
@@ -237,9 +200,9 @@ elif [ $# -eq 1 ] ; then  # if nothing is specified for $2, all test-cases shoul
             RESULT=$?
             set -e
 
+            # return a fail if there is an error when outputting the result value into the output
             if [[ "${RESULT}" -ne 0 ]] ; then
               # fail condition
-              # need to find a way to obtain the word 'instruction'
                echo "${TESTNAME} ${instruction} Fail"
                continue
                # exit
@@ -248,7 +211,6 @@ elif [ $# -eq 1 ] ; then  # if nothing is specified for $2, all test-cases shoul
             # we now need to extract the necessary lines with the prefix "RESULT : "
             # The associated value is then output and written into a .out file
             # so that we can compare it to the reference output result files
-        #    >&2 echo " Extracting final results"
             PATTERN="RESULT : "
             NOTHING=""
 
@@ -263,22 +225,16 @@ elif [ $# -eq 1 ] ; then  # if nothing is specified for $2, all test-cases shoul
             # Actual final result of the test case is stored in a .out file
             # Note that the output of this will have spaces before the actual value but
             # this shouldn't have an effect when comparing to the reference files
-        #done
-
-        #>&2 echo " Successfully ran the test-bench"
 
             >&2 echo " 4 - Comparing to the reference output files"
             # Here we compare the generated output files from part 3 with the pre-generated
             #  output files in 4-reference
 
-        # for i in ${TESTCASES} ; do
             TESTNAME=$(basename ${i} .asm.txt)
             set +e # +e used to stop the script failing if an error occurs
             >&2 diff -w test/4-reference/${TESTNAME}.out test/3-output/CPU_MU0_bus_${TESTNAME}.out
             RESULT=$? # output of this diff line stored in RESULT
             set -e
-
-            # need to capture the instruction name we are testing
 
             # Based on whether differences were found, either pass or fail
             if [[ "${RESULT}" -ne 0 ]] ; then
