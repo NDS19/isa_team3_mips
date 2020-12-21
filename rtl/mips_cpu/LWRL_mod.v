f959453c4d2ff9f57d317344d09317db908900e4
max-wickham <mw1919@ic.ac.uk>
2020-12-21 14:11:00 +0000

    LWR

--- rtl/mips_cpu/LWRL_mod.v
+++ rtl/mips_cpu/LWRL_mod.v
@@ -6,14 +6,14 @@ module LWRL_mod(
     output logic[31:0] LWR
 );
 
-assign LWL = (byte_number == 3'b001) & ({SrcA[31:24],SrcB[23:0]}) ||
-              (byte_number == 3'b010) & ({SrcA[31:16],SrcB[15:0]}) ||
-              (byte_number == 3'b011) & ({SrcA[31:8],SrcB[7:0]}) ||
-              (byte_number == 3'b100) & (SrcA);
+assign LWL = (byte_number == 3'b001)?32'hFFFF:32'h0000 & ({SrcA[31:24],SrcB[23:0]}) ||
+              (byte_number == 3'b010)?32'hFFFF:32'h0000 & ({SrcA[31:16],SrcB[15:0]}) ||
+              (byte_number == 3'b011)?32'hFFFF:32'h0000 & ({SrcA[31:8],SrcB[7:0]}) ||
+              (byte_number == 3'b100)?32'hFFFF:32'h0000 & (SrcA);
 
-assign LWR = (byte_number == 3'b001) & ({SrcB[31:8],SrcA[7:0]}) ||
-              (byte_number == 3'b010) & ({SrcB[31:16],SrcA[15:0]}) ||
-              (byte_number == 3'b011) & ({SrcB[31:24],SrcA[23:0]}) ||
-              (byte_number == 3'b100) & (SrcA);
+assign LWR = (byte_number == 3'b001)?32'hFFFF:32'h0000 & ({SrcB[31:8],SrcA[7:0]}) ||
+              (byte_number == 3'b010)?32'hFFFF:32'h0000 & ({SrcB[31:16],SrcA[15:0]}) ||
+              (byte_number == 3'b011)?32'hFFFF:32'h0000 & ({SrcB[31:24],SrcA[23:0]}) ||
+              (byte_number == 3'b100)?32'hFFFF:32'h0000 & (SrcA);
 
 endmodule
